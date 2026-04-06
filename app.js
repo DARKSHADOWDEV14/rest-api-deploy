dbf33aad85aa293212583184c89782cf4864737c
Andrew <andresdavid.dev.1405@gmail.com>
2026-04-06 08:32:15 -0500

    segundo

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.patch("/movies/:id", (req, res) => {
   return res.json(updateMovie);
 });
 
-const PORT = process.env.PORT ?? 1234;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`);
